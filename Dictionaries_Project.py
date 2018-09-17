45fbf72060ebf6df3f5cee4b81bd925139a5d70f
Zachery Reynolds <zachery.reynolds@stu.jefferson.kyschools.us>
2018-09-17 08:33:48 -0400

    commit

---
+++ Dictionaries_Project.py
@@ -0,0 +1,20 @@
+# Zachery Reynolds
+# Dictionary 09/17/18
+
+
+# Imports
+import time
+import sys
+import random
+
+# Start
+start = input("Welcome to Black Ops 3. Press 'f' to pay respec... I mean to start"):
+    if start == 'f':
+        print("*Ali A theme song*")
+    else:
+        break
+
+game = input("Would you like to access Campaign, Multiplayer, Zombies, or Bonus?"):
+    if game == 
+    
+    
